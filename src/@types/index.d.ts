b26a74cfa802a1d8c765a63e19cdb65333d4e004
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-02 02:43:50 +0900

    [CHORE] 초기 세팅 오타 수정

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,5 +2,5 @@ import type { COLOR_VARIANT } from "@/constants/global/colors";
 
 declare global {
   type etcColor = "etcA" | "etcB";
-  type color = keyof typeof COLOR_VARIANT | EtcColor;
+  type color = keyof typeof COLOR_VARIANT | etcColor;
 }
