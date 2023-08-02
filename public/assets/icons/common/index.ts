d426480a98d4790f29d474504dc48b2d5df1b749
jungbam <jungbam0809@gmail.com>
2023-08-03 04:30:19 +0900

    [FEAT] icon index 추가

---
+++ public/assets/icons/common/index.ts
@@ -0,0 +1,35 @@
+import alarm_light from "./alarm_light.png";
+import alarm_dark from "./alarm_dark.png";
+import close_light from "./close_light.png";
+import close_dark from "./close_dark.png";
+import eye_light from "./eye_light.png";
+import eye_dark from "./eye_dark.png";
+import heart_light from "./heart_light.png";
+import heart_dark from "./heart_dark.png";
+import mike_light from "./mike_light.png";
+import mike_dark from "./mike_dark.png";
+import profile_light from "./profile_light.png";
+import profile_dark from "./profile_dark.png";
+import search_light from "./search_light.png";
+import search_dark from "./search_dark.png";
+import star_empty from "./star_empty.png";
+import star_full from "./star_full.png";
+
+exports = {
+  alarm_light,
+  alarm_dark,
+  close_light,
+  close_dark,
+  eye_light,
+  eye_dark,
+  heart_light,
+  heart_dark,
+  mike_light,
+  mike_dark,
+  profile_light,
+  profile_dark,
+  search_light,
+  search_dark,
+  star_empty,
+  star_full,
+};
