be3066095d0b7b590f73ae7459727b038384e811
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-03 05:15:31 +0900

    [TEST] 나의 키워드, 인기 키워드 GET 요청 테스트

--- src/tests/msw/handlers.ts
+++ src/tests/msw/handlers.ts
@@ -1,1 +1,49 @@
-export const handlers = [];
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("/api/userCategories", (req, res, ctx) => {
+    // const VALID_TOKEN = "your-valid-token";
+    // const token = req.headers.get("Authorization");
+    // if (!token || token !== `Bearer ${VALID_TOKEN}`) {
+    //   return res(ctx.status(401), ctx.json({ error: "Unauthorized" }));
+    // }
+    return res(
+      ctx.status(200),
+      ctx.json([
+        "개발자",
+        "깃헙사용법정리",
+        "깃허브",
+        "코딩용어",
+        "알고리즘",
+        "Pascal",
+        "Object",
+        "IMP",
+        "Javascript",
+        "PEARL",
+        "JASS",
+        "PL/SQL",
+        "Java",
+        "Language",
+      ]),
+    );
+  }),
+  rest.get("/api/trendCategories", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        "Github",
+        "Java",
+        "Physon",
+        "IMP",
+        "Language",
+        "ALGOL",
+        "Javascript",
+        "PEARL",
+        "Object",
+        "PL/SQL",
+        "Pascal",
+        "JASS",
+      ]),
+    );
+  }),
+];
--- src/tests/units/KeywordSearch.test.tsx
+++ src/tests/units/KeywordSearch.test.tsx
@@ -5,14 +5,42 @@ import userEvent from "@testing-library/user-event";
 import RootLayout from "@/app/layout";
 import Home from "@/app/page";
 
-describe("키워드 검색 테스트", () => {
-  test("edit 버튼을 누르면 키워드 검색 모달이 열린다.", async () => {
+test("edit 버튼을 누르면 키워드 검색 모달이 열리고 Cancle 버튼을 누르면 닫힌다.", async () => {
+  render(
+    <RootLayout>
+      <Home />
+    </RootLayout>,
+  );
+  await userEvent.click(screen.getByRole("button", { name: /edit/ }));
+  expect(
+    await screen.findByRole("button", { name: "Cancle" }),
+  ).toBeInTheDocument();
+
+  await userEvent.click(screen.getByRole("button", { name: "Cancle" }));
+  expect(
+    screen.queryByRole("button", { name: "Cancle" }),
+  ).not.toBeInTheDocument();
+});
+
+describe("키워드 검색 모달 API 요청 테스트", () => {
+  beforeEach(async () => {
     render(
       <RootLayout>
         <Home />
       </RootLayout>,
     );
     await userEvent.click(screen.getByRole("button", { name: /edit/ }));
-    expect(await screen.findByText("Add my keywords")).toBeInTheDocument();
+  });
+
+  test("현재 나의 키워드 get 요청 테스트", async () => {
+    const expectedItemCount = 14;
+    const items = await screen.findAllByTestId(/my_/);
+    expect(items.length).toBe(expectedItemCount);
+  });
+
+  test("실시간 인기 키워드 get 요청 테스트", async () => {
+    const expectedItemCount = 12;
+    const items = await screen.findAllByTestId(/trend_/);
+    expect(items.length).toBe(expectedItemCount);
   });
 });
