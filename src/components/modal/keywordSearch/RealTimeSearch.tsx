5c979651eedf4fdfbf7b84907c7b23ba48ca91a0
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-04 05:38:59 +0900

    [FEAT] 실시간 검색 모달 키보드 이벤트 구현

--- src/components/modal/keywordSearch/RealTimeSearch.tsx
+++ src/components/modal/keywordSearch/RealTimeSearch.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 import Input from "@/components/designSystem/Input";
+import { BG_COLOR } from "@/constants/global/colors";
 
 import search from "../../../../public/assets/home/search.png";
 import NonPortalModal from "../NonPortalModal";
@@ -14,8 +15,9 @@ interface realtimeSearchProps {
 }
 
 const RealtimeSearch = ({ value, onChange }: realtimeSearchProps) => {
-  const modalRef = React.useRef<HTMLUListElement | null>(null);
   const [isModal, setIsModal] = React.useState<boolean>(false);
+  const [focusedIndex, setFocusedIndex] = React.useState<number | null>(null);
+  const modalRef = React.useRef<HTMLUListElement | null>(null);
   const inputRef = React.useRef<HTMLLabelElement | null>(null);
 
   const { data } = useQuery({
@@ -23,11 +25,34 @@ const RealtimeSearch = ({ value, onChange }: realtimeSearchProps) => {
     queryFn: () => axios.get("/api/liveSearch"),
   });
 
-  const closeOnOutsideClick = (event: MouseEvent) => {
+  const modalOutsideClick = React.useCallback((event: MouseEvent) => {
     if (!(event.target instanceof Node)) return;
     const isClickInsideInput = inputRef.current?.contains(event.target);
     const isClickInsideModal = modalRef.current?.contains(event.target);
     if (!isClickInsideInput && !isClickInsideModal) setIsModal(false);
+  }, []);
+
+  const searchKeywordClick = () => {
+    setIsModal((prev) => !prev);
+  };
+
+  const keyboardHandler = (event: React.KeyboardEvent) => {
+    const { key } = event;
+    const maxIndex = (data?.data.length || 0) - 1;
+
+    if (key === "ArrowDown") {
+      return setFocusedIndex((prev) =>
+        prev !== null && prev < maxIndex ? prev + 1 : 0,
+      );
+    }
+    if (key === "ArrowUp") {
+      return setFocusedIndex((prev) =>
+        prev !== null && prev > 0 ? prev - 1 : maxIndex,
+      );
+    }
+    if (key === "Enter" && focusedIndex !== null) {
+      return searchKeywordClick();
+    }
   };
 
   React.useEffect(() => {
@@ -35,12 +60,12 @@ const RealtimeSearch = ({ value, onChange }: realtimeSearchProps) => {
   }, [value]);
 
   React.useEffect(() => {
-    document.addEventListener("mousedown", closeOnOutsideClick);
-    return () => document.removeEventListener("mousedown", closeOnOutsideClick);
-  }, []);
+    document.addEventListener("mousedown", modalOutsideClick);
+    return () => document.removeEventListener("mousedown", modalOutsideClick);
+  }, [modalOutsideClick]);
 
   return (
-    <div className="w-full">
+    <div className="w-full" onKeyDown={keyboardHandler}>
       <label
         data-testid="realTimeInput"
         htmlFor="searchModal"
@@ -57,11 +82,16 @@ const RealtimeSearch = ({ value, onChange }: realtimeSearchProps) => {
           <NonPortalModal topLeft={{ top: 50, left: 19.5 }} noneBackdrop>
             <ul className="w-[1180px] shadow-xl" ref={modalRef}>
               {data?.data.map((result: string, i: number) => {
+                const bgColor =
+                  focusedIndex === i ? "bg-gray-200" : BG_COLOR.primary;
+
                 return (
                   <li
                     key={i}
-                    className={`flex items-center w-full h-[60px] bg-white cursor-pointer hover:bg-gray-200`}
-                    onClick={() => setIsModal((prev) => !prev)}
+                    data-testid={`item-${i}`}
+                    tabIndex={0}
+                    className={`flex items-center w-full h-[60px] ${bgColor} cursor-pointer hover:bg-gray-200`}
+                    onClick={searchKeywordClick}
                   >
                     <Image
                       className="px-6"
