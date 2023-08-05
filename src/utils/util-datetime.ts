7c1a0086d4a463b75a70155b46294e2a8139efd8
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-06 03:24:27 +0900

    [FEAT] 날짜 변환 유틸 함수 추가

---
+++ src/utils/util-datetime.ts
@@ -0,0 +1,22 @@
+const utilConvertTime = (createdAt: Date | string) => {
+  const creatTime = new Date(createdAt);
+  const now = new Date();
+
+  const timeGap = now.getTime() - creatTime.getTime();
+
+  if (timeGap < 24 * 60 * 60 * 1000) {
+    const hours = Math.round(timeGap / (60 * 60 * 1000));
+    return `${hours}시간 전`;
+  } else {
+    const year = creatTime.getUTCFullYear();
+    let month = creatTime.getUTCMonth() + 1;
+    let day = creatTime.getUTCDate();
+
+    if (month < 10) month = Number("0" + month);
+    if (day < 10) day = Number("0" + day);
+
+    return `${year}.${month}.${day}`;
+  }
+};
+
+export default utilConvertTime;
