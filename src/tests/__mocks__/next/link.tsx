3732e03a439e657169b3cb2205b44851828df16a
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-10 00:04:13 +0900

    [TEST] 내비게이션 기능 모킹

---
+++ src/tests/__mocks__/next/link.tsx
@@ -0,0 +1,19 @@
+/**
+ *
+ * @description
+ * useRouter에서 사용하는 mockPush를 사용해 Link도 자동 모킹되도록 했습니다.
+ *
+ */
+
+import React from "react";
+
+import { mockPush } from "./router";
+
+const NextLink: React.FC<{
+  children: React.ReactNode;
+  href: string;
+}> = ({ children, href }) => {
+  return <a onClick={() => mockPush(href)}>{children}</a>;
+};
+
+export default NextLink;
