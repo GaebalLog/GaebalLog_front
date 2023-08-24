84e5f3ffff4162608463b17725d7c4c10d1cfc9e
jungbam <jungbam0809@gmail.com>
2023-08-24 15:52:44 +0900

    [FEAT] 음성인식 관련 마이크 인식되지 않았을 때 처리,  마이크 음성인식되었을 때 처리

--- src/components/VoiceSearch.tsx
+++ src/components/VoiceSearch.tsx
@@ -9,9 +9,12 @@ interface props {
   setVoice: (value: string) => void;
 }
 const VoiceSearch: React.FC<props> = ({ closeModal, setVoice }) => {
-  const { transcript, resetTranscript, listening } = useSpeechRecognition();
+  const { isMicrophoneAvailable, transcript, resetTranscript, listening } =
+    useSpeechRecognition();
+
   const { getIcon } = useIcon();
-  const mike = getIcon("mike", 100, 100, "cursor hover");
+  const close = getIcon("close", 20, 20, "cursor hover");
+  const voiceSearch = getIcon("voiceSearch", 100, 100);
 
   const stopListening = () => {
     SpeechRecognition.stopListening();
@@ -19,7 +22,8 @@ const VoiceSearch: React.FC<props> = ({ closeModal, setVoice }) => {
     resetTranscript();
     closeModal();
   };
-  // 음성인식에 따른 처리
+
+  // 음성입력이 되고 1초마다 음성입력이 없으면 검색
   React.useEffect(() => {
     const interval = setInterval(() => {
       if (transcript !== "" && !listening) stopListening();
@@ -33,11 +37,31 @@ const VoiceSearch: React.FC<props> = ({ closeModal, setVoice }) => {
   }, []);
 
   return (
-    <div className={`w-[780px] h-[488px] ${BG_COLOR.general02}`}>
-      {listening && <div>Listening...</div>}
-      <span>{transcript}</span>
-      <button onClick={closeModal}>닫기</button>
-      {mike}
+    <div
+      className={`relative w-[780px] h-[488px] px-[24px] py-[30px] ${BG_COLOR.general02}`}
+    >
+      {!isMicrophoneAvailable && (
+        <div>
+          <h1>음성으로 검색</h1>
+          <h2>
+            음성으로 검색하려면 브라우저 설정으로 이동해 마이크에 대한 엑세스를
+            허용하세요.
+          </h2>
+        </div>
+      )}
+      {isMicrophoneAvailable && <div className="text-[24px]">듣는 중...</div>}
+      <span>검색어 : {transcript}</span>
+      <button onClick={closeModal} className="absolute top-[24px] right-[24px]">
+        {close}
+      </button>
+      <div className="absolute flex justify-center items-center bottom-[53px] left-[50%] translate-x-[-50%]">
+        {isMicrophoneAvailable && (
+          <span
+            className={`animate-ping absolute inline-flex h-[90px] w-[90px] rounded-full ${BG_COLOR.general07} opacity-75 -z-10`}
+          />
+        )}
+        {voiceSearch}
+      </div>
     </div>
   );
 };
