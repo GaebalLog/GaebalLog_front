7fa6fb7d181a927040fabcd6489b3ce294337b78
jhchoi1182 <jhchoi1182@gmail.com>
2023-08-25 21:12:39 +0900

    [FEAT] useInput 초기 상태값 설정할 수 있도록 변경

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,8 +1,8 @@
 import type React from "react";
 import { useState } from "react";
 
-const useInput = () => {
-  const [value, setValue] = useState("");
+const useInput = (initialState?: string | number) => {
+  const [value, setValue] = useState<string | number>(initialState ?? "");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
