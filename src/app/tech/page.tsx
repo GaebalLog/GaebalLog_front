eac9cb3dfbd9f779f526316b98e438863ca0ada9
jungbam <jungbam0809@gmail.com>
2023-08-31 17:41:27 +0900

    [FEAT] 글 목록 페이지 무한스크롤 구현

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { QUERY_KEYS } from "@/constants/global/querykeys";
 import Post from "@/components/commonUI/Post";
@@ -12,31 +12,53 @@ import type { postDetail } from "./[postId]/page";
 
 const TechPage = () => {
   const [tab, setTab] = React.useState<sortTab>("조회 순");
-  const [page, setPage] = React.useState<number>(1);
+  const loadMoreRef = React.useRef(null);
 
   const sort = tab === "조회 순" ? "views" : "created_at";
-  const { data } = useQuery({
-    queryKey: [QUERY_KEYS.POSTLIST_HOME, sort, page],
-    queryFn: async () => await postAPI.getAll(sort, page),
-  });
-  const { data: infinite } = useInfiniteQuery({
-    queryKey: [QUERY_KEYS.POSTLIST_HOME, sort, page],
-    queryFn: async () => await postAPI.getAll(sort, page),
-    getNextPageParam: (lastPage) => {
-      if (lastPage?.data.hasMore) return setPage((prev) => prev + 1);
-    },
-  });
-  console.log(infinite);
-  const postList = data?.data.posts;
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: [QUERY_KEYS.POSTLIST_HOME, sort],
+      queryFn: ({ pageParam = 1 }) => postAPI.getAll(sort, pageParam),
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage?.data.hasMore ? allPages.length + 1 : undefined;
+      },
+    });
+  const postList = React.useMemo(() => {
+    return data?.pages.flatMap((page) => page?.data.posts) || [];
+  }, [data]);
+  console.log(data);
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting && hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      },
+      {
+        threshold: 0.7,
+      },
+    );
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [hasNextPage, isFetchingNextPage]);
+
   return (
     <div className="w-[1632px] flex justify-between mt-[20px]">
       <TechSideBar />
       <div className="w-full">
         <SortBar tab={tab} setTab={setTab} />
-        <div className="flex flex-col items-end gap-[20px]">
+        <div className="relative flex flex-col items-end gap-[20px]">
           {postList?.map((post: postDetail) => {
             return <Post post={post} key={post.post_id} />;
           })}
+          <div ref={loadMoreRef} className="absolute bottom-[50px]" />
         </div>
       </div>
     </div>
