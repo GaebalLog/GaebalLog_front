dbfdc476ac0986117380583ad12ccb3dcf68b6d2
jungbam <jungbam0809@gmail.com>
2023-09-14 17:38:06 +0900

    [FIX] 화살표 색상 변경

--- src/components/detail/comment/element/card/ChildCommentCard.tsx
+++ src/components/detail/comment/element/card/ChildCommentCard.tsx
@@ -13,11 +13,11 @@ const ChildCommentCard: React.FC<
     deps === "child"
       ? `grid grid-cols-[auto,1fr] pt-4 px-[55px] mb-6 ${BG_COLOR.general01}`
       : `grid grid-cols-[auto,1fr] pt-4 pl-[125px] pr-[55px] mb-6 ${BG_COLOR.purple}`;
-
+  const color = deps === "grandchild" ? "#888888" : "#D3D3D3";
   return (
     <div className={styles}>
-      <ArrowInNestedComment className="mr-[22.6px]" />
-      <CommentCard {...comment} />
+      <ArrowInNestedComment className="mr-[22.6px]" color={color} />
+      <CommentCard {...comment} grandChildComment={deps === "grandchild"} />
     </div>
   );
 };
--- src/components/detail/comment/icons/ArrowInNestedComment.tsx
+++ src/components/detail/comment/icons/ArrowInNestedComment.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const ArrowInNestedComment: React.FC<{ className: string }> = ({
+const ArrowInNestedComment: React.FC<{ className: string; color?: string }> = ({
   className,
+  color,
 }) => {
   return (
     <svg
@@ -16,7 +17,7 @@ const ArrowInNestedComment: React.FC<{ className: string }> = ({
         fillRule="evenodd"
         clipRule="evenodd"
         d="M2 38L2 0H1.90735e-06L0 38V40H2H22.2L18.4004 43.7996L20.0004 45.3996L26.4004 38.9996L20.0004 32.5996L18.4004 34.1996L22.2008 38H2Z"
-        fill="#D3D3D3"
+        fill={color || "#D3D3D3"}
       />
     </svg>
   );
