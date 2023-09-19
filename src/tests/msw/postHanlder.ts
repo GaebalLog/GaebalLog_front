9acd504d0c06e2cf1e476a3d087c3b0b62e51a2e
jungbam <jungbam0809@gmail.com>
2023-09-20 02:55:36 +0900

    [FEAT] 글 작성 msw 핸들러 로직 추가

--- src/tests/msw/postHanlder.ts
+++ src/tests/msw/postHanlder.ts
@@ -2,42 +2,50 @@ import { rest } from "msw";
 
 import type { postDataType } from "@/api/postAPI";
 
+const posts = [
+  {
+    postId: 30,
+    nickname: "nickname",
+    title: "title",
+    content: "더",
+    view: null,
+    like: null,
+    img: ["img"],
+    categories: ["jwt", "aws", "mysql"],
+    createdDt: "2023-08-28 17:08:22",
+  },
+];
+const postList = [
+  {
+    postId: 37,
+    nickname: "카카오",
+    title: "디테일 페이지 제목",
+    content: "디테일 페이지 내용",
+    view: null,
+    like: null,
+    img: "test img update4",
+    categories: ["jwt", "aws", "mysql"],
+    isAuthor: true,
+    createdAt: "2023-08-28 17:08:22",
+  },
+  {
+    postId: 29,
+    nickname: "카카오1",
+    title: "디테일 페이지 제목",
+    content: "디테일 페이지 내용",
+    view: null,
+    like: null,
+    img: "test img update4",
+    categories: ["jwt", "aws", "mysql"],
+    isAuthor: false,
+    createdAt: "2023-08-28 17:08:22",
+  },
+];
 export const postHandler = [
   rest.get("/post/detail/:id", (req, res, ctx) => {
     const postId = req.params.id;
-    if (postId == "37") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          postId: 37,
-          nickname: "카카오",
-          title: "디테일 페이지 제목",
-          content: "디테일 페이지 내용",
-          view: null,
-          like: null,
-          img: "test img update4",
-          categories: ["jwt", "aws", "mysql"],
-          isAuthor: true,
-          createdAt: "2023-08-28 17:08:22",
-        }),
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          postId: 29,
-          nickname: "카카오1",
-          title: "디테일 페이지 제목",
-          content: "디테일 페이지 내용",
-          view: null,
-          like: null,
-          img: "test img update4",
-          categories: ["jwt", "aws", "mysql"],
-          isAuthor: false,
-          createdAt: "2023-08-28 17:08:22",
-        }),
-      );
-    }
+    const post = postList.find((post) => post.postId == +postId);
+    return res(ctx.status(200), ctx.json(post));
   }),
 
   rest.get("post/all/:sort", (req, res, ctx) => {
@@ -48,43 +56,24 @@ export const postHandler = [
         ctx.status(200),
         ctx.json({
           hasMore: true,
-          posts: [
-            {
-              postId: 30,
-              nickname: "nickname",
-              title: "title",
-              content: "더",
-              view: null,
-              like: null,
-              img: "img",
-              categories: ["jwt", "aws", "mysql"],
-              createdDt: "2023-08-28 17:08:22",
-            },
-          ],
+          posts: posts,
         }),
       );
     } else if (sort === "views" && page && page === "2") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          hasMore: false,
-          posts: [
-            {
-              postId: 37,
-              nickname: "nickname",
-              title: "title",
-              content: "마지막",
-              view: null,
-              like: null,
-              img: "img",
-              categories: ["jwt", "aws", "mysql"],
-              createdDt: "2023-08-28 17:08:22",
-            },
-          ],
-        }),
-      );
+      posts.push({
+        postId: 37,
+        nickname: "nickname",
+        title: "title",
+        content: "마지막",
+        view: null,
+        like: null,
+        img: ["img"],
+        categories: ["jwt", "aws", "mysql"],
+        createdDt: "2023-08-28 17:08:22",
+      });
+      return res(ctx.status(200), ctx.json({ hasMore: false, posts: posts }));
     } else {
-      return res(ctx.status(404));
+      return res(ctx.status(500));
     }
   }),
 
@@ -106,6 +95,18 @@ export const postHandler = [
     };
     const postData = req.body as postDataType;
     if (isCreatePost(postData)) {
+      console.log("in");
+      posts.push({
+        postId: 38,
+        nickname: "글 작성",
+        title: postData.title,
+        content: postData.content,
+        view: null,
+        like: null,
+        img: postData.img,
+        categories: postData.categories,
+        createdDt: "2023-08-28 17:08:22",
+      });
       return res(ctx.status(201));
     }
   }),
