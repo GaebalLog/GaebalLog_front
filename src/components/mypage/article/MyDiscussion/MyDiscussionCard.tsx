8586e345bbdc24f22eb3aae2f8f058be3d4a896a
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-08 15:01:56 +0900

    [FEAT] 마이페이지 토의 카드에 시작 전 추가

--- src/components/mypage/article/MyDiscussion/MyDiscussionCard.tsx
+++ src/components/mypage/article/MyDiscussion/MyDiscussionCard.tsx
@@ -6,9 +6,10 @@ import DateConvertor from "@/utils/util-dateConvertor";
 
 import Button from "../../../designSystem/Button";
 
+import ProgressStatus from "./ProgressStatus";
+
 const styles = {
   cardBox: `w-full h-[350px] relative flex items-center gap-20 px-[32px] ${BG_COLOR.primary} cursor-pointer`,
-  isDoneCircle: `absolute top-4 right-6 rounded-2xl py-1`,
   contentsWrapper: `flex flex-col gap-[24px]`,
   header: `flex items-center gap-[16px] ${TEXT_COLOR.general07rev}`,
   nickname: `${TEXT_COLOR.general07rev} text-[20px]`,
@@ -16,12 +17,15 @@ const styles = {
   categories: `flex items-center gap-[16px] absolute bottom-[24px]`,
 };
 
-const MyDiscussionCard: React.FC<{ discussion: beforeDiscussion }> = ({
+const MyDiscussionCard: React.FC<{ discussion: myDiscussion }> = ({
   discussion,
 }) => {
   const router = useRouter();
 
-  const dateConvertor = new DateConvertor(discussion.createdAt);
+  const localISOString = new DateConvertor(
+    discussion?.createdAt,
+  ).convertToLocalISOString();
+  const dateConvertor = new DateConvertor(localISOString);
 
   const onClickHandler: React.MouseEventHandler<HTMLDivElement> = (e) => {
     if (!(e.target instanceof HTMLElement)) {
@@ -29,44 +33,36 @@ const MyDiscussionCard: React.FC<{ discussion: beforeDiscussion }> = ({
     } else if (e.target.closest(".excluded")) {
       return;
     }
-    router.push(`/discussion/${discussion.chatListId}`);
+    router.push(`/discussion/${discussion.discussionId}`);
   };
 
   return (
     <div
       className={styles.cardBox}
       onClick={onClickHandler}
-      data-testid={`discussion${discussion.chatListId}`}
+      data-testid={`discussion${discussion.discussionId}`}
     >
-      <div
-        className={`${styles.isDoneCircle} ${
-          discussion.isDone
-            ? "px-4 bg-[#03A727] border-[#096B13]"
-            : "px-[10px] bg-[#FFA800] border-[#B86E00]"
-        }`}
-      >
-        {discussion.isDone ? "완료" : "진행 중"}
-      </div>
+      <ProgressStatus status={discussion.status} />
       <div className="h-[280px]">
         <div className={styles.contentsWrapper}>
           <div className={styles.header}>
-            <p className={styles.nickname}>{discussion.nickname}</p>
+            <p className={styles.nickname}>{discussion?.nickname}</p>
             <h2>{dateConvertor.formatWithLongDate()}</h2>
           </div>
           <h1 className={styles.title}>{discussion.title}</h1>
-          {!discussion.isDone && (
-            <span>{`남은 시간 ${discussion.remainingTime}`}</span>
+          {discussion.status === "end" && (
+            <span>{`남은 시간 ${discussion?.remainingTime}`}</span>
           )}
           <div className={styles.categories}>
-            {discussion.categories.map((category) => (
+            {/* {discussion.categories.map((category) => (
               <Button
-                key={`${discussion.chatListId}${category}`}
+                key={`${discussion.discussionId}${category}`}
                 color="grey"
                 size="tag"
               >
                 # {category}
               </Button>
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
