b1d3e23c0472843a454e76397f92bccb3fa044ce
jhchoi1182 <jhchoi1182@gmail.com>
2023-10-10 11:33:47 +0900

    [FEAT] 유저 상세페이지 토의 관리 API 연결

--- src/components/profile/article/UserDiscussion.tsx
+++ src/components/profile/article/UserDiscussion.tsx
@@ -1,28 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import React from "react";
 
 import SortBar from "@/components/commonUI/SortBar";
 import MyDiscussionCard from "@/components/mypage/article/MyDiscussion/MyDiscussionCard";
 import { QUERY_KEYS } from "@/constants/global/querykeys";
+import { authAPI } from "@/api/authAPI";
 
 const UserDiscussion: React.FC<{ params: string }> = ({ params }) => {
   const [tab, setTab] = React.useState<sortTab>("조회 순");
 
   const { data } = useQuery({
     queryKey: [QUERY_KEYS.NEIGHBORDISCUSSION, params],
-    queryFn: async () => await axios.get(`/api/users/discussion/${params}`),
+    queryFn: async () => await authAPI.userDiscussion(params),
   });
-  const discussionList = data?.data.discussions;
+  const discussionList = data?.data;
 
   return (
     <div className="flex w-full h-full flex-col px-[44px] py-[24px]">
       <SortBar tab={tab} setTab={setTab} option="mypage" />
       <article className="grid grid-cols-3 gap-[86px] mt-[11px] overflow-auto">
-        {discussionList?.map((discussion: beforeDiscussion) => {
+        {discussionList?.map((discussion: myDiscussion) => {
           return (
             <MyDiscussionCard
-              key={`discussion${discussion.chatListId}`}
+              key={`discussion${discussion.discussionId}`}
               discussion={discussion}
             />
           );
