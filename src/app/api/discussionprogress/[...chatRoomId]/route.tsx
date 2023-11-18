9a9852e3bc8635994bcc8527c7f6d05a79e2bea2
jhchoi1182 <jhchoi1182@gmail.com>
2023-11-19 00:00:51 +0900

    [REFACTOR] mock api 삭제

--- src/app/api/discussionprogress/[...chatRoomId]/route.tsx
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-  const discussionprogress =
-    "<div>스타일 컴포넌트 : 반영 가능하지만 무거워서 싫다는 의견이 다수라서 제외</div>";
-  return NextResponse.json(discussionprogress);
-};
